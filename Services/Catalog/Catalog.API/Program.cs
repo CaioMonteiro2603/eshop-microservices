e7d87fbd87d5d3937e235b470fa75d8912a5891e
Caio Monteiro <caiomonteiropro@gmail.com>
2024-07-18 11:44:11 -0300

    Microservices with Vertical Slice

--- Services/Catalog/Catalog.API/Program.cs
+++ Services/Catalog/Catalog.API/Program.cs
@@ -1,9 +1,16 @@
 var builder = WebApplication.CreateBuilder(args);
 
 //before builder = add services to the container 
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(typeof(Program).Assembly); // command necessary to request the application 
+}); 
 
-var app = builder.Build();
+var app = builder.Build();  
 
 // after builder = configure the HTTP request pipeline 
+
+app.MapCarter(); 
 
 app.Run();
