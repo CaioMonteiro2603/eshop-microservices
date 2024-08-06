4fe84dd576b67858ec1d7f7951f1e9d8edc842d4
Caio Monteiro <caiomonteiropro@gmail.com>
2024-08-06 19:45:13 -0300

    ordering domain's done

--- Services/Ordering/Ordering.Domain/Models/Order.cs
+++ Services/Ordering/Ordering.Domain/Models/Order.cs
@@ -17,4 +17,52 @@ public class Order : Aggregate<OrderId>
         get => OrderItems.Sum(x => x.Price * x.Quantity);
         private set { }
     }
+
+    public static Order Create(OrderId orderId, CustomerId customerId, OrderName orderName,
+        Address shippingAddress, Address billingAddress, Payment payment)
+    {
+        var order = new Order
+        {
+            Id = orderId,
+            CustomerId = customerId,
+            OrderName = orderName,
+            ShippingAddress = shippingAddress,
+            BillingAddress = billingAddress,
+            Payment = payment,
+            Status = OrderStatus.Pending
+        }; 
+
+        order.AddDomainEvents(new OrderCreatedEvent(order));
+
+        return order;
+    }
+
+    public void Update(OrderName orderName, Address shippingAddress, Address billingAddres, Payment payment, OrderStatus orderStatus)
+    {
+        OrderName = orderName;
+        ShippingAddress = shippingAddress;
+        BillingAddress = billingAddres;
+        Payment = payment;
+        Status = orderStatus;
+
+        AddDomainEvents(new OrderUpdatedEvent(this));
+    }
+
+    public void Add(ProductId productId, int quantity, decimal price)
+    {
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(quantity);
+        ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+        var orderItem = new OrderItem(Id, productId, quantity, price);
+        _orderItems.Add(orderItem);
+    }
+
+    public void Remove(ProductId productId)
+    {
+        var orderItem = _orderItems.FirstOrDefault(x => x.ProductId == productId);
+        if(orderItem is not null)
+        {
+            _orderItems.Remove(orderItem);
+        }
+    }
 }
--- Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
+++ Services/Ordering/Ordering.Domain/ValueObjects/OrderId.cs
@@ -3,4 +3,16 @@
 public record OrderId
 {
     public Guid Value { get; }
+    private OrderId(Guid value) => Value = value; // private constructor
+
+    public static OrderId Of(Guid value)
+    {
+        ArgumentNullException.ThrowIfNull(value);
+        if(value == Guid.Empty)
+        {
+            throw new DomainException("OrderId cannot be null");
+        }
+
+        return new OrderId(value);
+    }
 }
