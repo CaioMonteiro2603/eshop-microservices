b08b9a5b33c97006154bf394b7cdb82a3d9dbe5c
Caio Monteiro <caiomonteiropro@gmail.com>
2024-08-07 12:04:59 -0300

    Creating ordering.infra

---
+++ Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderConfiguration : IEntityTypeConfiguration<Order>
+{
+    public void Configure(EntityTypeBuilder<Order> builder)
+    {
+        
+    }
+}
---
+++ Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ordering.Infrastructure.Data.Configurations;
+
+public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+{
+    public void Configure(EntityTypeBuilder<OrderItem> builder)
+    {
+        
+    }
+}
