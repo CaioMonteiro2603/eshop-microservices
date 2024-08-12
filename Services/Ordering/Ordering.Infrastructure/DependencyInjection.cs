bc359129db5c930c12528aacd909799ba9f6a283
Caio Monteiro <caiomonteiropro@gmail.com>
2024-08-12 12:14:56 -0300

    Creating Ordering.Application

--- Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
+++ Services/Ordering/Ordering.Infrastructure/DependencyInjection.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Data;
 using Ordering.Infrastructure.Data.Interceptors;
 
 namespace Ordering.Infrastructure;
@@ -21,7 +22,8 @@ public static class DependencyInjection
             opt.AddInterceptors(serviceProvider.GetServices<ISaveChangesInterceptor>());
             opt.UseSqlServer(connectionString);
         });
-            
+
+        services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
         return services; 
     }
