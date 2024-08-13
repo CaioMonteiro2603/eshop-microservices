89c86896f2ce10788cac5e4b1324ff8d2f97ef0b
Caio Monteiro <caiomonteiropro@gmail.com>
2024-08-13 11:53:13 -0300

    Ordering Application Layer applied

---
+++ Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByCustomer/GetOrdersByCustomerQuery.cs
@@ -0,0 +1,7 @@
+﻿namespace Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+public record GetOrderByCustomerQuery(Guid CustomerId)
+    : IQuery<GetOrderByCustomerResult>;
+
+public record GetOrderByCustomerResult(IEnumerable<OrderDto> Orders); 
+
